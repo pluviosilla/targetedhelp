adf6f04f0512e60792ecfbaa59331b951136daeb
pauls <paul@thestrongfamily.org>
2010-06-09 01:34:12 -0500

    Switched gem management to bundler

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "http://rubygems.org"
+
+gem "rails", "2.3.8"
+gem "rack",  ">=1.1.0"
+gem "rake", "0.8.7"
+gem "haml", ">=3.0"
+gem "mysql", "2.8.1"
+
+gem "paperclip", "2.3.2"
+gem "authlogic", "2.1.5"
--- config/environment.rb
+++ config/environment.rb
@@ -21,8 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   #config.gem "warden"
   #config.gem "devise"
-  config.gem "haml"
-  config.gem "authlogic"
+  config.gem "bundler", :version => '>0.9.24'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
